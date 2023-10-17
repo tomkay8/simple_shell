26f2569c67aa1b744b90c480aa74ba50f31fa583
Kayode Thomas <thomaskayode8@gmail.com>
2023-10-17 04:02:23 -0700

    Shell project

--- simple_shell.c
+++ simple_shell.c
@@ -6,44 +6,71 @@
 #include <sys/wait.h>
 #include "simple_shell.h"
 
-int main() {
-    char command[MAX_COMMAND_LENGTH];
-    int status;
+/**
+ * main - Entry point for the simple shell program
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+	char command[MAX_COMMAND_LENGTH];
+	int status;
 
-    while (1) {
-        /* Display the prompt */
-        printf("$ ");
-        fflush(stdout);
+	while (1)
+	{
+		/* Display the prompt */
+		write(STDOUT_FILENO, "$ ", 2);
 
-        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
-            /* Check for end of file (Ctrl+D) */
-            printf("\n");
-            break;
-        }
+		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+		{
+			/* Check for end of file (Ctrl+D) */
+			write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
 
-        if (command[strlen(command) - 1] == '\n') {
-            /* Remove the trailing newline character */
-            command[strlen(command) - 1] = '\0';
-        }
+		if (command[strlen(command) - 1] == '\n')
+		{
+			/* Remove the trailing newline character */
+			command[strlen(command) - 1] = '\0';
+		}
 
-        execute_command(command, &status); /* Call the function with status variable */
-    }
+		execute_command(command, &status); /* Call the function */
+	}
 
-    return 0;
+	return (0);
 }
 
-void execute_command(char *command, int *status) {
-    pid_t pid = fork();
+/**
+ * execute_command - Execute a shell command
+ * @command: The command to execute
+ * @status: A pointer to the status variable
+ *
+ * This function forks child process and attempts to execute the given command.
+ * If it successful, the child process is replaced with the executed command.
+ *
+ * Return: 0
+ */
+void execute_command(char *command, int *status)
+{
+	pid_t pid = fork();
 
-    if (pid < 0) {
-        perror("Fork failed"); /* Print an error message for fork failure */
-        exit(1);
-    } else if (pid == 0) {
-        char *args[] = {command, NULL};  /* Array of pointers */
-        execve(command, args, NULL); /* Try to execute the command */
-        perror("Command not found"); /* Print an error message if the command is not found */
-        exit(1);
-    } else {
-        wait(status); /* Wait for the child process to finish */
-    }
+	if (pid < 0)
+	{
+		perror("Fork failed");
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		char *args[2];
+		args[0] = command;
+		args[1] = NULL;
+
+		execve(args[0], args, NULL); /* Try to execute the command */
+		perror("Command not found");
+		exit(1);
+	}
+	else
+	{
+		wait(status); /* Wait for the child process to finish */
+	}
 }
