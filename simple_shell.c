a9ba0526e1728712d14ecdb6d9d2d850131c893c
TFOdetayo <tayeodetayo@gmail.com>
2023-10-17 11:09:02 +0100

    update the header files

--- simple_shell.c
+++ simple_shell.c
@@ -1,27 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include "simple_shell.h"
 
-int main() {
+int main()
+{
     char command[MAX_COMMAND_LENGTH];
     int status;
 
-    while (1) {
+    while (1)
+    {
         /* Display the prompt */
         printf("$ ");
         fflush(stdout);
 
-        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
+        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+        {
             /* Check for end of file (Ctrl+D) */
             printf("\n");
             break;
         }
 
-        if (command[strlen(command) - 1] == '\n') {
+        if (command[strlen(command) - 1] == '\n')
+        {
             /* Remove the trailing newline character */
             command[strlen(command) - 1] = '\0';
         }
@@ -32,18 +30,24 @@ int main() {
     return 0;
 }
 
-void execute_command(char *command, int *status) {
+void execute_command(char *command, int *status)
+{
     pid_t pid = fork();
 
-    if (pid < 0) {
+    if (pid < 0)
+    {
         perror("Fork failed"); /* Print an error message for fork failure */
         exit(1);
-    } else if (pid == 0) {
-        char *args[] = {command, NULL};  /* Array of pointers */
-        execve(command, args, NULL); /* Try to execute the command */
-        perror("Command not found"); /* Print an error message if the command is not found */
+    }
+    else if (pid == 0)
+    {
+        char *args[] = {command, NULL}; /* Array of pointers */
+        execve(command, args, NULL);    /* Try to execute the command */
+        perror("Command not found");    /* Print an error message if the command is not found */
         exit(1);
-    } else {
+    }
+    else
+    {
         wait(status); /* Wait for the child process to finish */
     }
 }
--- simple_shell.h
+++ simple_shell.h
@@ -1,9 +1,17 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
+
+/* header files */
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 #define MAX_COMMAND_LENGTH 100
 #define MAX_ARGUMENTS 10
 
 void execute_command(char *command, int *status);
 
 #endif /* SIMPLE_SHELL_H */
-
