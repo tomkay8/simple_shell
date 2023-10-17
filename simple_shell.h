a096a6ebf4904c32edb4b8bc4a8c6cb91be4613a
Kayode Thomas <thomaskayode8@gmail.com>
2023-10-16 21:13:45 -0700

    shell

---
+++ simple_shell.h
@@ -0,0 +1,9 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+#define MAX_COMMAND_LENGTH 100
+#define MAX_ARGUMENTS 10
+
+void execute_command(char *command, int *status);
+
+#endif /* SIMPLE_SHELL_H */
+
---
+++ simple_shell_with_arguments.c
@@ -0,0 +1,61 @@
+#include "simple_shell.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    char command[MAX_COMMAND_LENGTH];
+    int status;
+
+    while (1) {
+        /* Display the prompt */
+        write(1, "$ ", 2);
+
+        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
+            /* Check for end of file (Ctrl+D) */
+            write(1, "\n", 1);
+            break;
+        }
+
+        if (command[strlen(command) - 1] == '\n') {
+            /* Remove the trailing newline character */
+            command[strlen(command) - 1] = '\0';
+        }
+
+        execute_command(command, &status); /* Call the function with the status variable */
+    }
+
+    return 0;
+}
+
+void execute_command(char *command, int *status) {
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        char *error_msg = "Fork failed\n";
+        write(2, error_msg, strlen(error_msg)); /* Print an error message for fork failure */
+        _exit(1);
+    } else if (pid == 0) {
+        char *args[MAX_ARGUMENTS + 2]; /* Array of pointers with space for arguments and two NULL pointers */
+        int i = 0;
+        char *token = strtok(command, " ");
+
+        while (token != NULL) {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+
+        args[i] = NULL;
+
+        if (execve(args[0], args, NULL) == -1) {
+            char *error_msg = "Command not found\n";
+            write(2, error_msg, strlen(error_msg)); /* Print an error message if the command is not found */
+            _exit(1);
+        }
+    } else {
+        wait(status); /* Wait for the child process to finish */
+    }
+}
