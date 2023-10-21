7e6f3eafd59a429b76cf91e6934ae2a0d2f2ab30
Kayode Thomas <thomaskayode8@gmail.com>
2023-10-21 13:00:36 -0700

    Updated

--- main.c
+++ main.c
@@ -1,46 +1,44 @@
 #include "shell.h"
 
-int main(void)
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
 {
-    char *error, *buffer = NULL;
-    size_t bufsize = 0;
-    ssize_t nread;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-    while (1)
-    {
-        char prompt[] = "$ "; /* Display the prompt */
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-        write(STDOUT_FILENO, prompt, 2);
-        nread = getline(&buffer, &bufsize, stdin);
-        if (nread == -1)
-        {
-            if (feof(stdin))
-            {
-                char newline = '\n';
-
-                write(STDOUT_FILENO, &newline, 1);
-                break;
-            }
-            error = "getline error\n"; /* Assign value here */
-            write(STDOUT_FILENO, error, 14);
-            exit(EXIT_FAILURE);
-        }
-        /* Remove the newline character */
-        if (buffer[nread - 1] == '\n')
-        {
-            buffer[nread - 1] = '\0';
-        }
-        if (isatty(STDIN_FILENO))
-        {
-            /* Execute the command */
-            if (execute_command(buffer) == -1)
-            {
-                error = "Command not found\n"; /* Assign value here */
-                write(STDOUT_FILENO, error, 18);
-            }
-        }
-    }
-    free(buffer);
-    return (0);
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
-
